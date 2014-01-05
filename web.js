7a0e438e10605bcee96c54c1a9f569f613cfbf92
Eduardo Del Balso <e.delbalso@gmail.com>
2014-01-04 21:49:58 -0500

    added api endpoint

--- web.js
+++ web.js
@@ -1,6 +1,15 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
+var dataStandardizations = require('./lib/dataStandardizations.js');
+
+app.get('/api/obesity.json', function(req, res) {
+  data = dataStandardizations.processAndReturnData();
+  response = JSON.stringify(data);
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.write(response);
+  res.end();
+});
 
 app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
